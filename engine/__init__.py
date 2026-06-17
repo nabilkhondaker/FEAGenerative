d1050f382d890ac615eb87783a27668da8ebbc3b
𝑵𝒂𝒃𝒊𝒍 𝑲𝒉𝒐𝒏𝒅𝒂𝒌𝒆𝒓 𝑨. <138402618+nabilkhondaker@users.noreply.github.com>
2026-06-16 19:38:30 -0700

    Create engine module with metadata
    Added core engine module with version and author info.

---
+++ engine/__init__.py
@@ -0,0 +1,8 @@
+"""
+Core Engine Module for Generative Design.
+Contains Solid Mechanics (Mesh/Elements), Finite Element Methods (FEM), 
+and Numerical Optimization (SIMP/OC) sub-packages.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Advanced Mechanics & Optimization Group"
